52a6e975497d8a20318768ca92dce54d9580fb5d
Rafael Rodrigues Antonio <rafaelrodriguesantonio12@gmail.com>
2026-07-02 16:17:06 -0300

    Delete Python directory

--- Python/03_data_types.py
+++
@@ -1,9 +0,0 @@
-nome = "Rafael"
-idade = 18
-altura = 1.75
-estudante = True
-
-print(type(nome))
-print(type(idade))
-print(type(altura))
-print(type(estudante))
--- Python/05_libraries.py
+++
@@ -1,5 +0,0 @@
-import math
-
-numero = 25
-
-print("Raiz quadrada:", math.sqrt(numero))
